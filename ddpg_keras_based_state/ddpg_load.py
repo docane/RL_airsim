a7b912636444dad20ebffa0572ce5447f7171c12
docane <alsgusrl06@gmail.com>
2023-05-27 10:58:21 +0900

    20230527

--- ddpg_keras_based_state/ddpg_load.py
+++ ddpg_keras_based_state/ddpg_load.py
@@ -1,8 +1,8 @@
 import gym
-from state_ddpg_8 import DDPGagent
+from state_ddpg_10 import DDPGagent
 import tensorflow as tf
 
-gym.envs.register(id='car_env-v0', entry_point='car_env_state_16_3:AirSimCarEnv')
+gym.envs.register(id='car_env-v0', entry_point='car_env_state_16_7:AirSimCarEnv')
 
 
 def main():
@@ -12,7 +12,12 @@ def main():
     # agent.load_weights('./models/airsim_ddpg_model_2023_04_05_13_11_57/')
     # agent.load_weights('./models/airsim_ddpg_model_2023_05_09_15_05_55/')
     # agent.load_weights('./models/airsim_ddpg_model_2023_05_24_15_10_24/')
-    agent.load_weights('./models/airsim_ddpg_model_2023_05_25_15_18_45/')
+    # agent.load_weights('./models/airsim_ddpg_model_2023_05_25_15_18_45/')
+    # agent.load_weights('./models/airsim_ddpg_model_2023_05_26_09_48_44/')
+    # agent.load_weights('./models/airsim_ddpg_model_2023_05_26_14_04_18/')
+    agent.load_weights('./models/airsim_ddpg_model_2023_05_27_02_25_46/')
+
+    #airsim_ddpg_model_2023_05_27_02_25_46
 
     state = env.reset()
 
